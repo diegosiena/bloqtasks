f721d4451e019c0d15accad74f0281afeb33b5a7
Diego Siena <diegosienafrancisco@hotmail.com>
2015-10-06 15:35:36 -0300

    Validates

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,6 @@
 class Client < ActiveRecord::Base
+  validates :name, presence: true
+
   has_many :project
 
   def to_s
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ActiveRecord::Base
+  validates :client_id, presence: true
+  validates :description, presence: true
+
   belongs_to :client
 end
