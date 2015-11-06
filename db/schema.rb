1c0d428975cf3ff7d1e5c6c94c7d66cdc7f9ca32
DIego Siena <diegosienafrancisco@hotmail.com>
2015-11-06 00:13:07 -0200

    Add Teams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029232601) do
+ActiveRecord::Schema.define(version: 20151106014813) do
 
   create_table "clients", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -39,6 +39,16 @@ ActiveRecord::Schema.define(version: 20151029232601) do
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
 
+  create_table "teams", force: :cascade do |t|
+    t.integer  "user_id",    limit: 4
+    t.integer  "project_id", limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "teams", ["project_id"], name: "index_teams_on_project_id", using: :btree
+  add_index "teams", ["user_id"], name: "index_teams_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
@@ -59,4 +69,6 @@ ActiveRecord::Schema.define(version: 20151029232601) do
 
   add_foreign_key "projects", "clients"
   add_foreign_key "tasks", "projects"
+  add_foreign_key "teams", "projects"
+  add_foreign_key "teams", "users"
 end
